fc55e7f292458214c03e96f61bef9d3f3119cfbe
ASIIGrupo6 <93481505+JrodDev95@users.noreply.github.com>
2021-10-31 09:57:40 -0600

    Add files via upload

---
+++ AplicacionWeb/config/db.php
@@ -0,0 +1,9 @@
+<?php
+/*Datos de conexion a la base de datos*/
+define('DB_HOST', 'localhost');//DB_HOST:
+define('DB_USER', 'root');//Usuario de tu base de datos
+define('DB_PASS', '');//Contraseña del usuario de la base de datos
+define('DB_NAME', 'bdproyectophp');//Nombre de la base de datos
+ 
+
+?>
